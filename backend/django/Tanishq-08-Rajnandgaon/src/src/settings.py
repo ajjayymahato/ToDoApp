72b2242ea3d7c28a304e0c7a4d358ea6698793c7
Tanishq Mishra <mishratanishq619@gmail.com>
2023-10-18 16:03:52 +0530

    new

--- backend/django/Tanishq-08-Rajnandgaon/src/src/settings.py
+++ backend/django/Tanishq-08-Rajnandgaon/src/src/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from decouple import config       # for .env
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p=s_2np4$c0#x%w_t6g&%!$^rj#3l^%g29_q8@7)zay65@vx3a'
+SECRET_KEY = str(config('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
