4298e4dfa8b725b7f8c43add22d706f74aba0053
Tanishq Mishra <mishratanishq619@gmail.com>
2023-10-18 23:37:16 +0530

    key-handled

--- backend/django/Tanishq-08-Rajnandgaon/src/src/settings.py
+++ backend/django/Tanishq-08-Rajnandgaon/src/src/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from decouple import config       # for .env
+# from decouple import config       # for .env
 
+import secrets
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(config('SECRET_KEY'))
+SECRET_KEY = secrets.token_hex(16)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
